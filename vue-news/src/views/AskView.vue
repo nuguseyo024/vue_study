9e8a3a4c268ae3caa7abb004e981043a2e7c38f6
nuguseyo024 <nuguseyo024@naver.com>
2022-09-02 09:17:05 +0900

    22-09-01 vue 학습

---
+++ vue-news/src/views/AskView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="item in ask" v-bind:key="item.id">{{ item.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchAskList } from "../api/index.js";
+
+export default {
+  data() {
+    return { ask: [] };
+  },
+  created() {
+    var vm = this;
+    fetchAskList()
+      .then(function (response) {
+        console.log(response);
+        vm.ask = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchNewsList } from "../api/index.js";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    var vm = this;
+    fetchNewsList()
+      .then(function (response) {
+        console.log(response);
+        vm.users = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style></style>
